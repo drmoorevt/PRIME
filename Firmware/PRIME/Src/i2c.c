97ba74bad2c02c6dddd63672b16e396396ae019b
Daniel Moore <drmoore@gmail.com>
2016-05-30 23:46:09 -0400

    1) I suppose it would help to commit the new drivers ...

---
+++ Firmware/PRIME/Src/i2c.c
@@ -0,0 +1,129 @@
+#include "stm32f4xx_hal.h"
+#include "i2c.h"
+#include "time.h"
+
+#define FILE_ID I2C_C
+
+I2C_HandleTypeDef *spI2C;
+
+/*****************************************************************************\
+* FUNCTION     I2C_init
+* DESCRIPTION  Initializes I2C support
+* PARAMETERS   none
+* RETURNS      nothing
+\*****************************************************************************/
+void I2C_init(I2C_HandleTypeDef *pI2C)
+{
+  spI2C = pI2C;
+  I2C_setup(false, I2C_CLOCK_RATE_400000);
+}
+
+/*****************************************************************************\
+* FUNCTION     I2C_init
+* DESCRIPTION  Initializes I2C support
+* PARAMETERS   none
+* RETURNS      nothing
+\*****************************************************************************/
+bool I2C_setup(boolean state, I2CClockRate rate)
+{
+  HAL_I2C_DeInit(spI2C);
+  if (state)
+  {
+    switch (rate)
+    {
+      case I2C_CLOCK_RATE_400000: spI2C->Init.ClockSpeed = 400000; break;
+      case I2C_CLOCK_RATE_200000: spI2C->Init.ClockSpeed = 200000; break;
+      case I2C_CLOCK_RATE_100000: spI2C->Init.ClockSpeed = 100000; break;
+      case I2C_CLOCK_RATE_050000: spI2C->Init.ClockSpeed = 50000;  break;
+      case I2C_CLOCK_RATE_025000: spI2C->Init.ClockSpeed = 25000;  break;
+      case I2C_CLOCK_RATE_012500: spI2C->Init.ClockSpeed = 12500;  break;
+      default: return FALSE;
+    }
+    HAL_I2C_Init(spI2C);
+  }
+  return true;
+}
+
+/*****************************************************************************\
+* FUNCTION     I2C_Send7bitAddress
+* DESCRIPTION  Writes numBytes beginning at pBytes to the I2C.
+* PARAMETERS   pBytes -- the bytes to write
+*              numBytes -- the number of bytes to write
+* RETURNS      nothing
+\*****************************************************************************/
+void I2C_send7bitAddress(uint8 address, uint8 direction)
+{
+  /* Test on the direction to set/reset the read/write bit */
+  if (direction != 0)
+    address |= I2C_OAR1_ADD0; /* Set the address bit0 for read */
+  else
+    address &= (uint8) ~ ((uint8)I2C_OAR1_ADD0); /* Reset the address bit0 for write */
+
+  I2C2->DR = address; /* Send the address */
+}
+
+/*****************************************************************************\
+* FUNCTION     I2C_write
+* DESCRIPTION  Writes numBytes beginning at pBytes to the I2C.
+* PARAMETERS   pBytes -- the bytes to write
+*              numBytes -- the number of bytes to write
+* RETURNS      nothing
+\*****************************************************************************/
+bool I2C_write(uint8_t devAddr, uint8 *pBytes, uint16 numBytes)
+{
+  bool retVal = (HAL_OK == HAL_I2C_Master_Transmit(spI2C, devAddr << 1, pBytes, numBytes, 100));
+  if (!retVal)
+    return false;
+  // Wait for the last byte and stop to be transmitted
+  uint32_t timeout = 1000;
+  while ((timeout-- > 0) && (spI2C->Instance->SR2 & I2C_SR2_BUSY))
+    Time_delay(1);
+  return (timeout > 0);
+}
+
+/*****************************************************************************\
+* FUNCTION     I2C_read
+* DESCRIPTION  Reads the specified number of bytes from the I2C and places them
+*              into the buffer pointed to by pBytes.
+* PARAMETERS   pBytes - buffer in which to the bytes read
+*              numBytes - the number of bytes to read
+* RETURNS      nothing
+\*****************************************************************************/
+bool I2C_read(uint8_t devAddr, uint8 *pBytes, uint16 numBytes)
+{
+  bool retVal = (HAL_OK == HAL_I2C_Master_Receive(spI2C, devAddr << 1, pBytes, numBytes, 100));
+  if (!retVal)
+    return false;
+  // Wait for the last byte and stop to be transmitted
+  uint32_t timeout = 1000;
+  while ((timeout-- > 0) && (spI2C->Instance->SR2 & I2C_SR2_BUSY))
+    Time_delay(1);
+  return (timeout > 0);
+}
+
+/*****************************************************************************\
+* FUNCTION     I2C_write
+* DESCRIPTION  Writes numBytes beginning at pBytes to the I2C.
+* PARAMETERS   pBytes -- the bytes to write
+*              numBytes -- the number of bytes to write
+* RETURNS      nothing
+\*****************************************************************************/
+void I2C_memWrite(uint8_t devAddr, uint16_t memAddr, uint8 *pBytes, uint16 numBytes)
+{
+  HAL_I2C_Master_Transmit(spI2C, devAddr << 1, pBytes, numBytes, 100);
+  while(spI2C->State != HAL_I2C_STATE_READY); // Wait for tx to complete
+}
+
+/*****************************************************************************\
+* FUNCTION     I2C_read
+* DESCRIPTION  Reads the specified number of bytes from the I2C and places them
+*              into the buffer pointed to by pBytes.
+* PARAMETERS   pBytes - buffer in which to the bytes read
+*              numBytes - the number of bytes to read
+* RETURNS      nothing
+\*****************************************************************************/
+void I2C_memRead(uint8_t devAddr, uint16_t memAddr, uint8 *pBytes, uint16 numBytes)
+{
+  HAL_I2C_Master_Receive(spI2C, devAddr << 1, pBytes, numBytes, 100);
+  while(spI2C->State != HAL_I2C_STATE_READY); // Wait for tx to complete
+}
---
+++ Firmware/PRIME/Src/i2c.h
@@ -0,0 +1,25 @@
+#ifndef I2C_H
+#define I2C_H
+
+#include "types.h"
+
+typedef enum
+{
+  I2C_CLOCK_RATE_400000 = 0,
+  I2C_CLOCK_RATE_200000 = 1,
+  I2C_CLOCK_RATE_100000 = 2,
+  I2C_CLOCK_RATE_050000 = 3,
+  I2C_CLOCK_RATE_025000 = 4,
+  I2C_CLOCK_RATE_012500 = 5,
+  I2C_CLOCK_RATE_MAX
+} I2CClockRate;
+
+void I2C_init(I2C_HandleTypeDef *pI2C);
+bool I2C_setup(boolean state, I2CClockRate rate);
+bool I2C_read(uint8_t devAddr, uint8 *pBytes, uint16 numBytes);
+bool I2C_write(uint8_t devAddr, uint8 *pBytes, uint16 numBytes);
+void I2C_memRead(uint8_t devAddr, uint16_t memAddr, uint8 *pBytes, uint16 numBytes);
+void I2C_memWrite(uint8_t devAddr, uint16_t memAddr, uint8 *pBytes, uint16 numBytes);
+
+
+#endif // I2C_H
