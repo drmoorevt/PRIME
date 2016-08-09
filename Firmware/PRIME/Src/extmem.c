693a2c99a4a32588dde6e55004f1d035afbb42d2
Daniel Moore <drmoore@gmail.com>
2016-08-08 23:05:24 -0400

    1) Adding the prelim doc and updating the ignore file. 2) Cleaning up / versioning some files of which we should keep track

---
+++ Firmware/PRIME/Src/extmem.c
@@ -0,0 +1,88 @@
+#include "stm32f4xx_hal.h"
+#include "extmem.h"
+
+#define REFRESH_COUNT           ((uint32_t)1386)   /* SDRAM refresh counter */
+#define SDRAM_TIMEOUT           ((uint32_t)0xFFFF)
+
+#define SDRAM_MODEREG_BURST_LENGTH_1             ((uint16_t)0x0000)
+#define SDRAM_MODEREG_BURST_LENGTH_2             ((uint16_t)0x0001)
+#define SDRAM_MODEREG_BURST_LENGTH_4             ((uint16_t)0x0002)
+#define SDRAM_MODEREG_BURST_LENGTH_8             ((uint16_t)0x0004)
+#define SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL      ((uint16_t)0x0000)
+#define SDRAM_MODEREG_BURST_TYPE_INTERLEAVED     ((uint16_t)0x0008)
+#define SDRAM_MODEREG_CAS_LATENCY_2              ((uint16_t)0x0020)
+#define SDRAM_MODEREG_CAS_LATENCY_3              ((uint16_t)0x0030)
+#define SDRAM_MODEREG_OPERATING_MODE_STANDARD    ((uint16_t)0x0000)
+#define SDRAM_MODEREG_WRITEBURST_MODE_PROGRAMMED ((uint16_t)0x0000)
+#define SDRAM_MODEREG_WRITEBURST_MODE_SINGLE     ((uint16_t)0x0200)
+
+void ExtMem_SDRAM_Initialization_sequence(uint32_t RefreshCount, SDRAM_HandleTypeDef *SdramHandle)
+{
+  FMC_SDRAM_CommandTypeDef Command;
+  __IO uint32_t tmpmrd =0;
+  
+  /* Step 1:  Configure a clock configuration enable command */
+  Command.CommandMode             = FMC_SDRAM_CMD_CLK_ENABLE;
+  Command.CommandTarget           = FMC_SDRAM_CMD_TARGET_BANK2;
+  Command.AutoRefreshNumber       = 1;
+  Command.ModeRegisterDefinition  = 0;
+
+  /* Send the command */
+  HAL_SDRAM_SendCommand(SdramHandle, &Command, SDRAM_TIMEOUT);
+
+  /* Step 2: Insert 100 us minimum delay */ 
+  /* Inserted delay is equal to 1 ms due to systick time base unit (ms) */
+  HAL_Delay(1);
+
+  /* Step 3: Configure a PALL (precharge all) command */ 
+  Command.CommandMode             = FMC_SDRAM_CMD_PALL;
+  Command.CommandTarget           = FMC_SDRAM_CMD_TARGET_BANK2;
+  Command.AutoRefreshNumber       = 1;
+  Command.ModeRegisterDefinition  = 0;
+
+  /* Send the command */
+  HAL_SDRAM_SendCommand(SdramHandle, &Command, SDRAM_TIMEOUT);  
+  
+  /* Step 4: Configure an Auto Refresh command */ 
+  Command.CommandMode             = FMC_SDRAM_CMD_AUTOREFRESH_MODE;
+  Command.CommandTarget           = FMC_SDRAM_CMD_TARGET_BANK2;
+  Command.AutoRefreshNumber       = 4;
+  Command.ModeRegisterDefinition  = 0;
+
+  /* Send the command */
+  HAL_SDRAM_SendCommand(SdramHandle, &Command, SDRAM_TIMEOUT);
+  
+  /* Step 5: Program the external memory mode register */
+  tmpmrd = (uint32_t)SDRAM_MODEREG_BURST_LENGTH_1          |
+                     SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL   |
+                     SDRAM_MODEREG_CAS_LATENCY_3           |
+                     SDRAM_MODEREG_OPERATING_MODE_STANDARD |
+                     SDRAM_MODEREG_WRITEBURST_MODE_SINGLE;
+  
+  Command.CommandMode             = FMC_SDRAM_CMD_LOAD_MODE;
+  Command.CommandTarget           = FMC_SDRAM_CMD_TARGET_BANK2;
+  Command.AutoRefreshNumber       = 1;
+  Command.ModeRegisterDefinition  = tmpmrd;
+
+  /* Send the command */
+  HAL_SDRAM_SendCommand(SdramHandle, &Command, SDRAM_TIMEOUT);
+  
+  /* Step 6: Set the refresh rate counter */
+  /* Set the device refresh rate */
+  HAL_SDRAM_ProgramRefreshRate(SdramHandle, RefreshCount); 
+}
+
+bool ExtMem_testSDRAM(void)
+{
+  uint32_t i;  // Avoid stomping on LCD SDRAM during the test by starting at BUFFER_OFFSET
+  for (i = BUFFER_OFFSET; i < SDRAM_DEVICE_SIZE; i++)
+  {
+    *(uint8_t *)(SDRAM_DEVICE_ADDR + i) = i;
+  }
+  for (i = BUFFER_OFFSET; i < SDRAM_DEVICE_SIZE; i++)
+  {
+    if (*(uint8_t *)(SDRAM_DEVICE_ADDR + i) != (i & 0x000000FF))
+      return false;
+  }
+  return true;
+}
---
+++ Firmware/PRIME/Src/extmem.h
@@ -0,0 +1,13 @@
+#ifndef EXTMEM_H
+#define EXTMEM_H
+
+#include "types.h"
+
+#define SDRAM_DEVICE_ADDR         ((uint32_t)0xD0000000)
+#define SDRAM_DEVICE_SIZE         ((uint32_t)0x800000)  /* SDRAM device size in MBytes */
+#define BUFFER_OFFSET             ((uint32_t)0x50000)   /* Offset beyond the LCD frame buffer */
+
+bool ExtMem_testSDRAM(void);
+void ExtMem_SDRAM_Initialization_sequence(uint32_t RefreshCount, SDRAM_HandleTypeDef *SdramHandle);
+
+#endif
